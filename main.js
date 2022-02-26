2d0740d61978b0088e4e1a5fd22985ca2ca8915c
Radivoje Katanic <k.rade313@gmail.com>
2022-02-26 12:45:10 +0100

    Updates

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 if (localStorage.getItem("dark-mode")) {
   document.body.classList.add("day-mode");
 }
-
-if (!isNaN(parseInt(window.localStorage.getItem("color")?.valueOf()))) {
-  window.document.documentElement.style.setProperty(
-    "--color-hue",
-    parseInt(window.localStorage.getItem("color")?.valueOf()).toString()
-  );
-}
